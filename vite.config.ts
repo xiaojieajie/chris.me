fdc839b5e2daf951880e9fb01891d01a1e2e1e75
chriszhu <1633711653@qq.com>
2021-01-05 15:37:16 +0800

    init new blog

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+const { resolve } = require('path')
+
+const pathResolve = (src: string) => resolve(__dirname, src)
+
+const pathName = '/blog'
+
+module.exports = {
+  port: 1111,
+  base: '.',
+  alias: {
+    '/@/': pathResolve('src'),
+    '/@c/': pathResolve('src/components'),
+  },
+  outDir: pathResolve(`../build${pathName}`),
+  // optimizeDeps: {
+  //   include: ['ant-design-vue', '@ant-design-vue/use', '@ant-design/icons-vue', 'vue-socket.io'],
+  // },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:1324/api',
+      changeOrigin: true,
+      rewrite: (path: string) => path.replace(/^\/api/, '')
+    }
+  }
+}
