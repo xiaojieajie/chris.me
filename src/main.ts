3b491b22a2d8672d31ac4193470fa101eb2f2bee
chriszhu <1633711653@qq.com>
2021-03-11 18:15:15 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import { Lazyload } from '@vant/lazyload'
 import 'element-plus/lib/theme-chalk/index.css'
 import 'element-plus/lib/theme-chalk/display.css'
 import '@/styles/main.scss'
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(Lazyload)
 app.use(router).use(store).mount('#app')
